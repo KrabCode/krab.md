ab0db47c072d00d9d8ad47c3fef1c6a31f23951e
Krab <jakubpodtrzitkorak@gmail.com>
2023-06-09 17:21:40 +0200

    url configurable settings

--- sketches/grid/main.js
+++ sketches/grid/main.js
@@ -1,5 +1,29 @@
+let settings = {
+    "weight" : 2.5,
+    "ptColor" : 120,
+    "bgColor" : 36,
+    "step" : 20,
+    "offset" : 10
+}
+
+function parseUrlParams() {
+    let keys = Object.keys(settings);
+    console.log("this sketch accepts the following optional url parameters with some sane defaults: " + keys);
+    console.log("example url: https://krab.md/sketches/grid?step=30&offset=15")
+    let urlParams = new URLSearchParams(window.location.href);
+    for(let i in keys){
+        let key = keys[i];
+        if(urlParams.has(key)){
+            settings[key] = parseFloat(urlParams.get(key));
+        }
+    }
+    console.log("settings after parsing url params:");
+    console.dir(settings);
+}
+
 function setup() {
-    createCanvas(windowWidth, windowHeight, WEBGL);
+    createCanvas(windowWidth, windowHeight);
+    parseUrlParams();
     drawDotGrid();
 }
 
@@ -7,13 +31,12 @@ function draw() {
 }
 
 function drawDotGrid() {
-    background(36);
-    translate(-width/2, -height/2);
+    background(settings.bgColor);
     noFill();
-    stroke(120);
-    strokeWeight(0.5);
-    let step = 20;
-    let offset = step / 2;
+    stroke(settings.ptColor);
+    strokeWeight(settings.weight);
+    let offset = settings.offset;
+    let step = settings.step;
     for (let x = offset; x < width; x+= step) {
         for (let y = offset; y < height; y+= step) {
             point(x,y);
