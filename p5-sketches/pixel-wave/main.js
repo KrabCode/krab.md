d543f7b9389ba2507a328b39280dec54531326ac
Krab <jakubpodtrzitkorak@gmail.com>
2023-06-16 11:15:35 +0200

    cooler landing page i guess preparations for npm and typescript

---
+++ p5-sketches/pixel-wave/main.js
@@ -0,0 +1,15 @@
+
+// https://p5js.org/
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    // update();
+}
+
+function draw() {
+    background(PARAMS.background);
+    // update();
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
