e35c2be80a6b4288e8724fdc7b2c889c76453cdd
Jakub Rak <rak@fss.eu>
2023-06-26 17:01:15 +0200

    nice line grid, trying to make ts work and failing

--- p5-sketches/dot-grid/main.js
+++ p5-sketches/dot-grid/main.js
@@ -1,49 +1,47 @@
-"use strict";
+// https://cocopon.github.io/tweakpane/
+const pane = new Tweakpane.Pane();
 
-let gridSettings = {
+let PARAMS = {
     "weight" : 2.5,
+    "margin" : 10,
+    "offset" : 10,
     "step" : 20,
-    "ptColor" : 120,
-    "bgColor" : 36
+    "foreground" : '#787878',
+    "background" : '#242424'
 }
 
-let parseUrlParams = () => {
-    let keys = Object.keys(gridSettings);
-    console.log("this sketch accepts these optional url parameters: " + keys);
-    let thisUrl = new URL(window.location);
-    let urlParams = new URLSearchParams(thisUrl.search);
-    console.log("thisUrl", thisUrl);
-    console.log("example url: " + thisUrl.origin + thisUrl.pathname + "?step=30&weight=3&bgColor=36&ptColor=255,0,150");
-    for(let i in keys){
-        let key = keys[i];
-        if(urlParams.has(key)){
-            let val = urlParams.get(key);
-            console.log("found param " + key + " : " + val);
-            if(key.toLowerCase().includes("color")){
-                let rgb = val.split(',');
-                console.log("rgb", rgb);
-                if(rgb.length > 2){
-                    gridSettings[key] = color(rgb[0], rgb[1], rgb[2]);
-                }else{
-                    gridSettings[key] = color(parseFloat(rgb[0]));
-                }
-            }else{
-                gridSettings[key] = parseFloat(val);
-            }
+let constraints = new Map();
+constraints.set('step', {min: 20, max: 100, step: 1});
 
-        }
-    }
-    console.log("gridSettings after parsing url params: " + JSON.stringify(gridSettings));
 
-    //
-    var entire = (drawDotGrid).toString();
-    var body = entire.substring(entire.indexOf("{") + 1, entire.lastIndexOf("}"));
-    console.log("grid code in p5.js: ", body);
+
+const f = pane.addFolder({
+    title: 'dot-grid-gui',
+    expanded: true,
+});
+
+const btn = f.addButton({
+    title: 'png',
+    label: 'save',   // optional
+});
+
+let paramNames = Object.keys(PARAMS);
+for(let i = 0; i < paramNames.length; i++){
+    let name = paramNames[i];
+    f.addInput(PARAMS, name, constraints.get(name));
 }
 
+btn.on('click', () => {
+    save("dot-grid.png");
+});
+
+f.on('change', function(ev){
+    drawDotGrid();
+});
+
+// https://p5js.org/
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    parseUrlParams();
     drawDotGrid();
 }
 
@@ -57,14 +55,15 @@ function draw() {
 }
 
 function drawDotGrid() {
-    background(gridSettings.bgColor);
+    background(PARAMS.background);
     noFill();
-    stroke(gridSettings.ptColor);
-    strokeWeight(gridSettings.weight);
-    let offset = gridSettings.step / 2;
-    let step = gridSettings.step;
-    for (let x = offset; x < width; x+= step) {
-        for (let y = offset; y < height; y+= step) {
+    stroke(PARAMS.foreground);
+    strokeWeight(PARAMS.weight);
+    let margin = PARAMS.margin;
+    let offset = PARAMS.offset;
+    let step = PARAMS.step;
+    for (let x = -margin+offset; x < width+offset+margin*2; x+= step) {
+        for (let y = -margin+offset; y < height+offset+margin*2; y+= step) {
             point(x,y);
         }
     }
