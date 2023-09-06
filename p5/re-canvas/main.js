14f2ecbc0c88aedcee8c77c7db35bd9116a0a48c
Krab <jakubpodtrzitkorak@gmail.com>
2023-09-06 21:32:06 +0200

    smoothed the canvas, ended the darkening

--- p5/re-canvas/main.js
+++ p5/re-canvas/main.js
@@ -1,7 +1,7 @@
 // https://p5js.org/
 let c;
-let thetaRangeHalf = 3.1415 * 0.2;
-let scaleRangeHalf = 0.3;
+let thetaRangeHalf = 3.1415 * 0.5;
+let scaleRangeHalf = 0.5;
 let thetaValue = 0.1;
 let scaleValue = 1.1;
 
@@ -14,12 +14,13 @@ function setup() {
     colorMode(RGB, 1, 1, 1, 1);
     imageMode(CENTER);
     noSmooth();
-    c = createGraphics(w, h);
+    let dim = Math.sqrt(width*width + height*height);
+    c = createGraphics(dim, dim);
     c.background(0);
     c.imageMode(CENTER);
     c.rectMode(CENTER);
     c.colorMode(RGB, 1, 1, 1, 1);
-    c.noSmooth()
+    // c.noSmooth()
 }
 
 function draw() {
@@ -37,7 +38,7 @@ function draw() {
         c.blendMode(DIFFERENCE);
         c.noStroke();
         c.fill(1 / 255);
-        c.rect(0, 0, c.width, c.height);
+        // c.rect(0, 0, c.width, c.height);
     }
     // draw the canvas on itself scaled and rotated
     c.scale(scaleValue);
@@ -51,7 +52,7 @@ function draw() {
     c.stroke(clrA.x, clrA.y, clrA.z);
     c.noFill();
     c.rect(0, 0, rectSize, rectSize);
-    let clrB = getColorAt((t + 0.5) % 1);
+    let clrB = getColorAt((t + 0.3) % 1);
     c.stroke(clrB.x, clrB.y, clrB.z);
     c.noFill();
     c.rotate(QUARTER_PI);
@@ -73,10 +74,10 @@ function isPointInsideCanvas(x, y) {
 
 // gradients adapted from glsl: https://iquilezles.org/articles/palettes/
 function getColorAt(time) {
-    let a = createVector(0.5, 0.5, 0.5);
-    let b = createVector(0.5, 0.5, 0.5);
-    let c = createVector(1.0, 1., 1.);
-    let d = createVector(0.00, 0.10, 0.20);
+    let a = createVector(0.8, 0.5, 0.4);
+    let b = createVector(0.2, 0.4, 0.2);
+    let c = createVector(2.0, 1.0, 1.0);
+    let d = createVector(0.00, 0.25, 0.25);
     let t = createVector(time, time, time);
     let tau = createVector(6.28318, 6.28318, 6.28318);
     //  a + b*cos( 6.28318*(c*t+d) );
